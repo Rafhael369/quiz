7ed420372d8df41ba07f3b3153b7ddd1e9052451
Guilherme <guilhermesz873@gmail.com>
2023-11-01 21:33:30 -0300

    Subindo front

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from quiz_app import Quiz
 
+quiz = Quiz()
 app = Flask(__name__)
 CORS(app)
-quiz = Quiz()
 
 @app.route("/temas")
 def temas():
     return quiz.temas()
 
-@app.route("/perguntas/<tema>")
+@app.route("/perguntas/<tema>", methods=["GET"])
 def trazer_perguntas(tema):
+    print(quiz.perguntas("Python"))
     return quiz.perguntas(tema)
 
 @app.route("/finalizar", methods=["GET"])
