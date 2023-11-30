40622acd84bc72903de4cb0425bb7996e097f951
Laboratório <=>
2023-11-29 21:57:47 -0300

    Ajustes

--- backend/app.py
+++ backend/app.py
@@ -2,21 +2,25 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from quiz import Quiz
 
-quiz = Quiz()
+# quiz = Quiz()
 app = Flask(__name__)
 CORS(app)
 
 
 @app.route("/correcao/<pergunta_id>/<resposta>")
 def corrigir(pergunta_id, resposta):
+    global quiz
     return quiz.correcao(pergunta_id, resposta)
 
 @app.route("/temas")
 def temas():
+    global quiz
+    quiz = Quiz()
     return quiz.temas()
 
 @app.route("/perguntas/<tema>/<quantidade>", methods=["GET"])
 def trazer_perguntas(tema, quantidade):
+    global quiz
     return quiz.perguntas(tema, quantidade)
 
 @app.route("/finalizar", methods=["GET"])
